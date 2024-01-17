7924fa8613cf052e8704bc0db6d3e871035195d5
gopalspatil <gopatil2004@gmail.com>
2024-01-17 15:51:47 +0530

    [ADDED]:Gopal c Program For Addition

---
+++ addition.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c=0;
+	printf("\n Enter The 1 Numder:");
+	scanf("%d",&a);
+	
+	printf("\n Enter The 2 Numder:");
+	scanf("%d",&b);
+	
+	c=a+b;
+	printf("\nAddition=%d",c);
+	
+	return 0;
+}
